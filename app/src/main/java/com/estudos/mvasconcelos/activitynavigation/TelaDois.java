bc0f43c6daec7103a08cacd8b844f6a40929bc9f
maxrjj <maxrjj@hotmail.com>
2016-04-20 17:41:35 -0300

    activity´s created

--- app/src/main/java/com/estudos/mvasconcelos/activitynavigation/TelaDois.java
+++ app/src/main/java/com/estudos/mvasconcelos/activitynavigation/TelaDois.java
@@ -1,19 +1,13 @@
 package com.estudos.mvasconcelos.activitynavigation;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TelaDois extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tela_dois);
     }
-
-    private Context getContext (){return this;}
-
-
-
 }
