60ed2371109cb998dc4f7a1a7537b2ff9afa0024
maxrjj <maxrjj@hotmail.com>
2016-04-20 18:27:51 -0300

    foram escritos os metodos para passagem de parametros entre as activitys

--- app/src/main/java/com/estudos/mvasconcelos/activitynavigation/MainActivity.java
+++ app/src/main/java/com/estudos/mvasconcelos/activitynavigation/MainActivity.java
@@ -1,8 +1,11 @@
 package com.estudos.mvasconcelos.activitynavigation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends DebugActivity {
 
@@ -10,10 +13,25 @@ public class MainActivity extends DebugActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btTela = (Button)findViewById(R.id.btTela1);
+        btTela.setOnClickListener(onClickBt());
+
+    }
+
+    private View.OnClickListener onClickBt(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText texto = (EditText)findViewById(R.id.txtTela1);
+                Intent intent = new Intent(getContext(),TelaDois.class);
+                Bundle params = new Bundle();
+                params.putString("nome",texto.getText().toString());
+                intent.putExtras(params);
+                startActivity(intent);
+            }
+        };
+
     }
 
     private Context getContext (){return this;}
-
-
-
 }
--- app/src/main/java/com/estudos/mvasconcelos/activitynavigation/TelaDois.java
+++ app/src/main/java/com/estudos/mvasconcelos/activitynavigation/TelaDois.java
@@ -2,6 +2,7 @@ package com.estudos.mvasconcelos.activitynavigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TelaDois extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class TelaDois extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_dois);
+
+        //recebe o nome enviado por parametro
+        Bundle args = getIntent().getExtras();
+        String nome = args.getString("nome");
+
+        TextView text = (TextView)findViewById(R.id.txtViewRecebe);
+        text.setText(nome);
+
     }
 }
